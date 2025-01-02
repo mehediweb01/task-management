3a06df8d9044681d610d98a37360b0c245c0dd97
First-project <mehedihasan0937580@gmail.com>
2025-01-03 01:54:07 +0600

    ItemsCard design done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 function App() {
   return (
     <>
-      <div className="h-screen bg-gunmetal flex justify-center items-center">
-        <div className="bg-sky shadow-md shadow-yellow-200 w-full sm:w-1/2 mx-6 rounded-full">
-          <h1 className="text-center text-4xl text-primary font-bold font-redHadDisplay">
-            demo text
-          </h1>
+      <div className="h-screen bg-gunmetal flex justify-center items-start pt-6">
+        <div className="bg-sky shadow-md shadow-yellow-200  sm:w-3/5 w-full  mx-6 rounded-xl py-4 px-2">
+          <div className="border-b border-slate-300 rounded-full">
+            <h1 className="text-center text-4xl text-primary font-semibold font-robotoSlab my-2 tracking-wide">
+              Task Management Apps
+            </h1>
+          </div>
         </div>
       </div>
     </>
---
+++ src/components/ItemsCard.tsx
@@ -0,0 +1,50 @@
+import { MdDeleteOutline } from "react-icons/md";
+import { FaRegEdit } from "react-icons/fa";
+const ItemsCard = ({
+  toggle,
+  title,
+  Delete,
+  edit,
+  editId,
+  listId,
+  editText,
+  date,
+}) => {
+  return (
+    <li className="flex items-center justify-between gap-2 mx-4 border-b border-slate-400 my-2">
+      <div className="flex items-center gap-3 my-1">
+        <input
+          type="checkbox"
+          className="size-7 accent-sky-600 focus:accent-sky-700"
+        />
+        {editId !== listId ? (
+          <form className="flex gap-2 items-center">
+            <input
+              type="text"
+              value={editText}
+              className="rounded-md focus:outline-lime-300 focus:border-none px-2 w-full font-redHadDisplay"
+            />
+          </form>
+        ) : (
+          <h2 className="font-redHadDisplay text-base font-semibold text-blackGreen w-full flex flex-col">
+            <span className={toggle && "line-through"}> {title} </span>
+            <span className="font-robotoSlab font-light text-sm">{date}</span>
+          </h2>
+        )}
+      </div>
+      <p className="bg-yellow-300 px-2 rounded-md font-semibold font-robotoSlab tracking-widest">
+        {toggle ? "complete" : "inprogress"}
+      </p>
+      <div className="flex items-center gap-1">
+        <button type="button" onClick={edit}>
+          <FaRegEdit className="size-5 text-blackGreen hover:text-green-300 transition-colors duration-300" />
+        </button>
+        <button type="button" onClick={Delete}>
+          <MdDeleteOutline className="size-5 hover:text-red-600 transition-colors duration-300" />
+        </button>
+      </div>
+    </li>
+  );
+};
+
+export default ItemsCard;
