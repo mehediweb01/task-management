36305d62a30be9888f375441601e64cf5321dc12
First-project <mehedihasan0937580@gmail.com>
2025-01-03 22:35:36 +0600

    functionality done

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,104 @@
-import InputField from "./components/InputField";
+import React, { useState } from "react";
+import ItemsCard from "./components/ItemsCard";
+
+interface IFace {
+  Id: number;
+  text: string;
+}
 
 function App() {
+  const [arr, setArr] = useState<IFace[]>([]);
+  const [Text, setText] = useState<string>("");
+  const [EditText, setEditText] = useState<string>("");
+  const [EditId, setEditId] = useState<number | null>(null);
+  const [toggle, setToggle] = useState<boolean>(false);
+
+  const AddTasks = (e: React.FormEvent) => {
+    e.preventDefault();
+    const TrimText = Text.trim();
+    if (TrimText === "") {
+      return;
+    } else {
+      setArr([...arr, { Id: Date.now(), text: TrimText }]);
+      setText("");
+    }
+  };
+
+  const handleEdit = (types: IFace) => {
+    setEditId(types.Id);
+    setEditText(types.text);
+  };
+
+  const handleUpdate = (id: number, e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const trimValue: string = EditText.trim();
+    if (!trimValue) return;
+    setArr(
+      arr.map((todo) => (todo.Id === id ? { ...todo, text: trimValue } : todo))
+    );
+    setEditId(null);
+    setEditText("");
+  };
+
+  const handleCancel = () => {
+    setEditId(null);
+    setText("");
+  };
+
+  const Delete = (id: number) => {
+    setArr(arr.filter((item) => item.Id !== id));
+  };
+  const toggleComplete = () => {
+    setToggle(!toggle);
+  };
   return (
     <>
       <div className="h-screen bg-gunmetal flex justify-center items-start pt-6">
         <div className="bg-sky shadow-md shadow-yellow-200  md:w-3/5 w-full rounded-xl py-4 px-2 mx-4">
           <div className="border-b border-slate-300 rounded-full">
-            <h1 className="text-center sm:text-4xl text-xl text-primary font-semibold font-robotoSlab my-2 tracking-wide">
+            <h1 className="text-center sm:text-4xl text-xl text-primary font-semibold font-robotoSlab my-2 tracking-wIde">
               Task Management Apps
             </h1>
           </div>
           <div className="py-4">
-            <InputField edit={false} />
+            <div>
+              <form className="flex sm:flex-row flex-col items-center gap-3 justify-center mb-6">
+                <input
+                  type="text"
+                  value={Text}
+                  placeholder="Enter your Task"
+                  onChange={(e) => setText(e.target.value)}
+                  className="px-2 py-2 rounded-md focus:outline-teal-300 focus:shadow-inner focus:shadow-indigo-700 outline outline-slate-500 font-robotoSlab text-xl tracking-wide text-blackGreen"
+                />
+
+                <div className="flex gap-2">
+                  <button
+                    onClick={AddTasks}
+                    className="bg-blackGreen text-primary px-4 py-2 rounded-lg font-redHadDisplay tracking-wide text-xl"
+                  >
+                    Add Task
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div>
+            {arr.map((item) => (
+              <ItemsCard
+                key={item.Id}
+                Delete={() => Delete(item.Id)}
+                handleEdit={() => handleEdit(item)}
+                editId={EditId}
+                listId={item.Id}
+                title={item.text}
+                editText={EditText}
+                handleCancel={handleCancel}
+                handleUpdate={(e: any) => handleUpdate(item.Id, e)}
+                EditOnChange={(e: any) => setEditText(e.target.value)}
+                toggleComplete={toggleComplete}
+                toggle={toggle}
+              />
+            ))}
           </div>
         </div>
       </div>
--- src/components/ItemsCard.tsx
+++ src/components/ItemsCard.tsx
@@ -1,43 +1,70 @@
 import { MdDeleteOutline } from "react-icons/md";
 import { FaRegEdit } from "react-icons/fa";
 const ItemsCard = ({
-  toggle,
   title,
   Delete,
-  edit,
+  handleEdit,
+  editText,
   editId,
   listId,
-  editText,
-  date,
-}) => {
+  handleCancel,
+  handleUpdate,
+  EditOnChange,
+  toggleComplete,
+  toggle,
+}: any) => {
+  const date = new Date();
+  const finalDate: string = date.toLocaleDateString();
   return (
     <li className="flex items-center justify-between gap-2 mx-4 border-b border-slate-400 my-2">
       <div className="flex items-center gap-3 my-1">
         <input
           type="checkbox"
           className="size-7 accent-sky-600 focus:accent-sky-700"
+          onClick={toggleComplete}
         />
-        {editId !== listId ? (
-          <form className="flex gap-2 items-center">
+        {editId === listId ? (
+          <form className="flex flex-col gap-2 items-start">
             <input
               type="text"
               value={editText}
+              onChange={EditOnChange}
+              autoFocus
               className="rounded-md focus:outline-lime-300 focus:border-none px-2 w-full font-redHadDisplay"
             />
+            <div className="flex justify-center items-center gap-3">
+              <button
+                className="bg-blackGreen text-primary px-2 py-1 rounded-lg font-redHadDisplay tracking-wide text-xl"
+                onClick={handleUpdate}
+              >
+                Save
+              </button>
+              <button
+                className="bg-red-600 text-white px-2 py-1 rounded-lg font-redHadDisplay text-xl"
+                onClick={handleCancel}
+              >
+                cancel
+              </button>
+            </div>
+            <span className="font-robotoSlab font-light text-sm">
+              {finalDate}
+            </span>
           </form>
         ) : (
           <h2 className="font-redHadDisplay text-base font-semibold text-blackGreen w-full flex flex-col">
-            <span className={toggle && "line-through"}> {title} </span>
-            <span className="font-robotoSlab font-light text-sm">{date}</span>
+            <span className={toggle ? "line-through" : undefined}>{title}</span>
+            <span className="font-robotoSlab font-light text-sm">
+              {finalDate}
+            </span>
           </h2>
         )}
       </div>
       <div className="flex sm:flex-row flex-col items-center gap-1 sm:gap-6">
         <p className="bg-yellow-300 px-2 rounded-md font-semibold font-robotoSlab tracking-widest">
-          {toggle ? "complete" : "inprogress"}
+          {toggle ? "completed" : "inprogress"}
         </p>
         <div className="flex items-center gap-1">
-          <button type="button" onClick={edit}>
+          <button type="button" onClick={handleEdit}>
             <FaRegEdit className="size-5 text-blackGreen hover:text-green-300 transition-colors duration-300" />
           </button>
           <button type="button" onClick={Delete}>
